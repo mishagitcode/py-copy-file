6b0c5be4751d375bcae25b040e33df7d0cd0e897
Misha Zubritsky <zubritsky.misha@gmail.com>
2025-08-15 16:47:07 +0300

    implemented py-copy-file v2

--- app/main.py
+++ app/main.py
@@ -5,11 +5,16 @@ def copy_file(command: str) -> None:
     if command != "":
         file_names = command.split(" ")
 
-        if len(file_names) < 3 or file_names[0] != "cp":
+        if len(file_names) != 3 or file_names[0] != "cp":
             return
 
         source_file = file_names[1]
         target_file = file_names[2]
+
+        if "/" in source_file or "/" in target_file:
+            return
+        if "\\" in source_file or "\\" in target_file:
+            return
 
         if os.path.exists(source_file) and source_file != target_file:
             with (open(source_file, "r") as file_in,
