101bb440734ba1b5c688670776ef9ebccf4da5a8
Misha Zubritsky <zubritsky.misha@gmail.com>
2025-08-15 16:38:04 +0300

    implemented py-copy-file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
-# write your code here
+import os
+
+
+def copy_file(command: str) -> None:
+    if command != "":
+        file_names = command.split(" ")
+
+        if len(file_names) < 3 or file_names[0] != "cp":
+            return
+
+        source_file = file_names[1]
+        target_file = file_names[2]
+
+        if os.path.exists(source_file) and source_file != target_file:
+            with (open(source_file, "r") as file_in,
+                  open(target_file, "w") as file_out):
+                content = file_in.read()
+                file_out.write(content)
